4ddfe4a09e515187ea387e13af918999aa191bf7
freddysilber <freddy.silber@gmail.com>
2021-11-13 15:34:41 -0700

    formatting

--- handler/handler.go
+++ handler/handler.go
@@ -13,18 +13,18 @@ var dbInstance db.Database
 
 func NewHandler(db db.Database) http.Handler {
 	router := chi.NewRouter()
-	
+
 	router.Use(cors.Handler(cors.Options{
 		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
 		AllowedOrigins: []string{"https://*", "http://*"},
 		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
-		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders: []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
 		// ExposedHeaders:   []string{"Link"},
 		AllowCredentials: false,
 		// MaxAge:           300, // Maximum value not ignored by any of major browsers
 	}))
-	
+
 	dbInstance = db
 
 	router.MethodNotAllowed(methodNotAllowedHandler)
